63a2949b1bcb796e33d81ecc0c321035f126f87d
Administrator <liberalzhou@163.com>
2019-03-18 10:43:08 +0800

    add eclipse.config

--- 2019-3-18/src/Example15.java
+++ 2019-3-18/src/Example15.java
@@ -4,23 +4,37 @@ public class Example15 {
 		Animal c = new Cat();
 		Animal d = new Dog();
 
+		Cat c2 = new Cat();
+
 		c.shout();
 		d.shout();
 
-		System.out.println("a=" + c.a);
+		System.out.println("a=" + c.a + ":" + c2.a);
 
 		System.out.println("b=" + c.b);
 
 	}
 }
 
+// abstract class
 abstract class Animal {
-	final int a = 5;
+	//public String a;
+	
+	public static final String a = "null";
+	
+	//final int a = 5;
 	float b = 20f;
 
 	public abstract void shout();
+
+	public void say() {
+
+	}
+
+	// public abstract void speak();
 }
 
+// extends class
 class Cat extends Animal {
 
 	final int a = 502;
@@ -33,8 +47,15 @@ class Cat extends Animal {
 
 	}
 
+	@Override
+	public void say() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
 
+// extends class
 class Dog extends Animal {
 
 	@Override
