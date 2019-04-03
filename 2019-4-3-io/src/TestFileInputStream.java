5c98d5a549dd8721fd1fdf61f84e2f574532e24f
Administrator <Administrator@A503-1-057>
2019-04-03 09:41:31 +0800

    add eclipse.config

--- 2019-4-3-io/src/TestFileInputStream.java
+++ 2019-4-3-io/src/TestFileInputStream.java
@@ -1,8 +1,11 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.CharBuffer;
 
 public class TestFileInputStream {
 
@@ -13,7 +16,7 @@ public class TestFileInputStream {
 					"a.txt"));
 			int b = 0;
 			while ((b = fileInputStream.read()) != -1) {
-				System.out.println((char) b);
+				System.out.print((char) b);
 			}
 
 			byte[] s = new byte[1024];
@@ -33,12 +36,27 @@ public class TestFileInputStream {
 			fileOutputStream.close();
 
 			// /***********************************
-			byte[] p = new byte[1024];
-			FileInputStream f2 = new FileInputStream(new File("a.jpg"));
-			f2.read(p);
 
-			FileOutputStream f3 = new FileOutputStream(new File("pic"), true);
-			f3.write(p);
+			byte[] p = new byte[1024 * 10];
+
+			FileInputStream f2 = new FileInputStream(new File("a.jpg"));
+			FileOutputStream f3 = new FileOutputStream(("pic/j.jpg"));
+
+			while (f2.read() != -1) {
+				f2.read(p);
+
+				f3.write(f2.read());
+
+				f3.write(p, 0, p.length);
+				f3.flush();
+
+			}
+
+			f2.read(p);
+			f3.write(p, 0, p.length);
+			f3.flush();
+			// f2.close();
+			// f3.close();
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
